857dbfa0817b3a2518db7f17e1cda2df2ac5e35c
Lukasz Buczynski <pirogronian0@gmail.com>
2021-01-07 21:10:53 +0100

    Further enhancing accumulators and its tests.

---
+++ src/Tests/AccumulatorTest.cpp
@@ -0,0 +1,53 @@
+
+#include <cassert>
+#include <Corrade/Utility/Debug.h>
+#include <Corrade/Interconnect/Receiver.h>
+#include <Utils/Accumulator.h>
+
+using namespace Corrade::Utility;
+using namespace Corrade::Interconnect;
+using namespace MOSP::Utils;
+
+struct Trec : public Receiver
+{
+    void recv(double d)
+    {
+        Debug{} << formatString("Trec::recv({})", d);
+    }
+};
+
+int main()
+{
+    StandardAccumulator acc;
+
+    assert(acc.accumulatedDelta() == 0);
+    assert(acc.referenceDelta() == 0);
+
+    Debug{} << acc;
+
+    acc.referenceDelta() = 10;
+
+    while(acc.accumulatedDelta() < acc.referenceDelta())
+    {
+        Debug{} << acc.appendDelta(1);
+        Debug{} << acc;
+    }
+
+    acc.reset();
+
+    while(acc.accumulatedDelta() < acc.referenceDelta())
+    {
+        acc.appendDelta(0.75);
+    }
+
+    Debug{} << acc;
+
+    StandardTriggeringAccumulator tacc(5);
+    Trec trec;
+    Corrade::Interconnect::connect(tacc, &StandardTriggeringAccumulator::triggered, trec, &Trec::recv);
+
+    for(int i = 0; i < 10; i++)
+        tacc.appendDelta(1);
+
+    return 0;
+}
--- src/Utils/Accumulator.h
+++ src/Utils/Accumulator.h
@@ -2,6 +2,9 @@
 #pragma once
 
 #include <Corrade/Interconnect/Emitter.h>
+#include <Corrade/Utility/FormatStl.h>
+#include <Corrade/Utility/DebugStl.h>
+#include <Corrade/Utility/Debug.h>
 
 namespace MOSP
 {
@@ -10,13 +13,15 @@ namespace MOSP
         template <typename T>
         class Accumulator
         {
+        protected:
             T m_accumulatedDelta{0};
             T m_referenceDelta{0};
         public:
+            Accumulator(T rd = 0) { m_referenceDelta = rd; }
             T accumulatedDelta() const { return m_accumulatedDelta; }
             T referenceDelta() const { return m_referenceDelta; }
             T& referenceDelta() { return m_referenceDelta; }
-            T left() const { m_referenceDelta - m_accumulatedDelta; }
+            T left() const { return m_referenceDelta - m_accumulatedDelta; }
             T appendDelta(T d) {
                 m_accumulatedDelta += d;
                 return left();
@@ -28,13 +33,31 @@ namespace MOSP
         class TriggeringAccumulator : public Accumulator<T>, public Corrade::Interconnect::Emitter
         {
         public:
+            TriggeringAccumulator(T rd) : Accumulator<T>(rd) {}
             Signal triggered(T dv)
             {
                 return emit(&TriggeringAccumulator::triggered, dv);
+            }
+            T appendDelta(T d)
+            {
+                T lft = Accumulator<T>::appendDelta(d);
+                if (lft <= 0)
+                {
+                    triggered(Accumulator<T>::m_accumulatedDelta);
+                    Accumulator<T>::reset();
+                }
+                return lft;
             }
         };
 
         typedef Accumulator<double> StandardAccumulator;
         typedef TriggeringAccumulator<double> StandardTriggeringAccumulator;
+
+        template<typename T>
+        Corrade::Utility::Debug& operator<<(Corrade::Utility::Debug& out, const Accumulator<T>& acc)
+        {
+            out << Corrade::Utility::formatString("Accumulator{{{}, {}}}", acc.accumulatedDelta(), acc.referenceDelta());
+            return out;
+        }
     }
 }
